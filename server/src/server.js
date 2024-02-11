9ce0cd74a3fa65267ce4ec8c3c7a7b77aad03933
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-02-11 11:08:03 +0600

    Middleware

--- server/src/server.js
+++ server/src/server.js
@@ -3,10 +3,13 @@ const  morgan = require("morgan");
 const  app= express();
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 const isLoggedIn =(req, res,next) =>{
-    const login = false;
+    const login = true;
     if (login){
+        req.body.id=101;
         next()
     }else {
         return res.status(401).json({message:'please login first'});
@@ -20,6 +23,7 @@ app.get("/test",(req,res)=>{
 });
 
 app.get("/api/user",isLoggedIn,(req,res)=>{
+    console.log(req.body.id)
     res.status(200).send({
         message:'User Profile'
     });
