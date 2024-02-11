46d0f82e5a5c35b33485998063afa07ef6536575
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-02-11 09:47:08 +0600

    Update

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,37 @@
 const  express = require("express");
+const  morgan = require("morgan");
 const  app= express();
 
+app.use(morgan('dev'));
 app.get("/test",(req,res)=>{
     res.status(200).send({
         message:'api is working fineee'
     });
 });
-app.get("/products",(req,res)=>{
+
+
+app.post("/test",(req,res)=>{
     res.status(200).send({
-        message:'product are returned'
+        message:'Post: api is working fineee'
     });
 });
+
+app.put("/test",(req,res)=>{
+    res.status(200).send({
+        message:'Update: api is working fineee'
+    });
+});
+
+app.delete("/test",(req,res)=>{
+    res.status(200).send({
+        message:'Delete: api is working fineee'
+    });
+});
+// app.get("/products",(req,res)=>{
+//     res.status(200).send({
+//         message:'product are returned'
+//     });
+// });
 
 app.listen(3001,()=>{
     console.log(`server is running at http://localhost:3001`);
