d3459eb84b76e52209759c12e6b8b5ee0bdcb288
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-02-11 08:49:42 +0600

    Update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const  express = require("express");
 const  app= express();
 
+app.get("/",(req,res)=>{
+    res.send('Welcome to the Server');
+})
+app.get("/products",(req,res)=>{
+    res.send('product are returned');
+})
+
 app.listen(3001,()=>{
     console.log(`server is running at http://localhost:3001`);
 });
